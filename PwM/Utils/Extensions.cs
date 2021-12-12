aa2144905c890704a694a77f24330937226bea8c
0x78654C <l2xguard@live.com>
2021-12-12 22:46:10 +0200

    added extension for split string by text

---
+++ PwM/Utils/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PwM.Utils
+{
+    internal static class Extensions
+    {
+        /// <summary>
+        /// Split string by a specific text.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="parameter"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        internal static string SplitByText(this string input, string parameter, int index)
+        {
+            string[] output = input.Split(new string[] { parameter }, StringSplitOptions.None);
+            return output[index];
+        }
+    }
+}
