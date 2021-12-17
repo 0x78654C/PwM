56a50adf49633969298eb9515c0bd490ddae8b7b
0x78654C <l2xguard@live.com>
2021-12-17 21:29:22 +0200

    fix startup (resource file issue)

---
+++ PwM/AddApplications.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PwM
+{
+    /// <summary>
+    /// Interaction logic for AddApplications.xaml
+    /// </summary>
+    public partial class AddApplications : Window
+    {
+        public AddApplications()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Mouse window drag function
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+
+        /// <summary>
+        /// Label button function for minimiza window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void miniMizeLBL_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+    }
+}
