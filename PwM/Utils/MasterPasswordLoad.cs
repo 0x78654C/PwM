ef7e3e6529eb4e4ec0663f55e52b3f435a6466c8
0x78654C <l2xguard@live.com>
2021-12-17 23:21:56 +0200

    moved load masterpassword from main to separate class

---
+++ PwM/Utils/MasterPasswordLoad.cs
@@ -0,0 +1,24 @@
+﻿using System.Security;
+
+namespace PwM.Utils
+{
+    public class MasterPasswordLoad
+    {
+        /// <summary>
+        /// Load master password form MasterPassword form after confirmation.
+        /// </summary>
+        /// <param name="vaultName"></param>
+        /// <returns></returns>
+        public static SecureString LoadMasterPassword(string vaultName)
+        {
+            SecureString password;
+            GlobalVariables.vaultName = vaultName;
+            MasterPassword masterPassword = new MasterPassword();
+            masterPassword.ShowDialog();
+            password = masterPassword.masterPassword;
+            masterPassword.masterPasswordPWD.Clear();
+            masterPassword.masterPassword = null;
+            return password;
+        }
+    }
+}
