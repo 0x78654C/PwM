fbc73193169dece6375e2a9a2b5286d05cb21dc5
0x78654C <l2xguard@live.com>
2021-12-17 23:51:47 +0200

    added class for clear global variables

---
+++ PwM/Utils/ClearVariables.cs
@@ -0,0 +1,18 @@
+﻿
+namespace PwM.Utils
+{
+    public class ClearVariables
+    {
+        /// <summary>
+        /// Clear global variables.
+        /// </summary>
+        public static void VariablesClear()
+        {
+            GlobalVariables.applicationName = "";
+            GlobalVariables.accountName = "";
+            GlobalVariables.accountPassword = "";
+            GlobalVariables.closeAppConfirmation = "";
+            GlobalVariables.deleteConfirmation = "";
+        }
+    }
+}
