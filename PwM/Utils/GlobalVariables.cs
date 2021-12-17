8517ecc9ec12218db6be21de23b616eb50a48521
0x78654C <l2xguard@live.com>
2021-12-17 23:52:01 +0200

    added new global variables

--- PwM/Utils/GlobalVariables.cs
+++ PwM/Utils/GlobalVariables.cs
@@ -9,10 +9,10 @@ namespace PwM.Utils
         public static string accountName { get; set; }
         public static string accountPassword { get; set; }
         public static string deleteConfirmation { get; set; }
+        public static string closeAppConfirmation { get; set; }
         public static string vaultName { get; set; }
         public static string gridColor { get; set; }
         public static string messageData { get; set; }
-        public static string decyrpt { get; set; }
         private static string s_rootPath = System.IO.Path.GetPathRoot(Environment.SystemDirectory);
         private static readonly string s_accountName = Environment.UserName;
         public static string passwordManagerDirectory = $"{s_rootPath}Users\\{s_accountName}\\AppData\\Local\\PwM\\";
