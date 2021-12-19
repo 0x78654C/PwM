9ef892bc5e1554a63baf0f41e01037a6de57bc18
0x78654C <l2xguard@live.com>
2021-12-20 00:41:10 +0200

    added new variables for vault checks/ vault confirm creation and counts

--- PwM/Utils/GlobalVariables.cs
+++ PwM/Utils/GlobalVariables.cs
@@ -9,10 +9,13 @@ namespace PwM.Utils
         public static string accountName { get; set; }
         public static string accountPassword { get; set; }
         public static string deleteConfirmation { get; set; }
+        public static string createConfirmation { get; set; }
         public static string closeAppConfirmation { get; set; }
         public static string vaultName { get; set; }
+        public static int vaultsCount { get; set; }
         public static string gridColor { get; set; }
         public static string messageData { get; set; }
+        public static bool vaultChecks = false;
         private static string s_rootPath = System.IO.Path.GetPathRoot(Environment.SystemDirectory);
         private static readonly string s_accountName = Environment.UserName;
         public static string passwordManagerDirectory = $"{s_rootPath}Users\\{s_accountName}\\AppData\\Local\\PwM\\";
