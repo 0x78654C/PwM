fe114652d3148caf0f933d6cec106bbdeda58c47
0x78654C <l2xguard@live.com>
2021-12-20 00:42:37 +0200

    added Delete vault window pop dialog

---
+++ PwM/DeleteVault.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+namespace PwM
+{
+    /// <summary>
+    /// Interaction logic for DeleteVault.xaml
+    /// </summary>
+    public partial class DeleteVault : Window
+    {
+        public DeleteVault()
+        {
+            InitializeComponent();
+            string vault = Utils.GlobalVariables.vaultName;
+            notificationLBL.Text = $"Do you want tot delete {vault} vault?";
+        }
+
+        /// <summary>
+        /// Close button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void confirmBTN_Click(object sender, RoutedEventArgs e)
+        {
+            Utils.GlobalVariables.deleteConfirmation = "yes";
+            this.Close();
+        }
+
+        private void CancelBTN_Click(object sender, RoutedEventArgs e)
+        {
+            Utils.GlobalVariables.deleteConfirmation = "";
+            this.Close();
+        }
+    }
+}
