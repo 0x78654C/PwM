3b577be67440295f464b679f95efd38d6ae6367a
0x78654C <l2xguard@live.com>
2021-12-20 22:21:08 +0200

    added class for control color listviewItems for tab menu

---
+++ PwM/Utils/ListViewSettings.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace PwM.Utils
+{
+    public class ListViewSettings
+    {
+        /// <summary>
+        /// Set Color of listView on click. 
+        /// </summary>
+        /// <param name="listViewItem"></param>
+        /// <param name="reset"></param>
+        public static void SetListViewColor(ListViewItem listViewItem, bool reset)
+        {
+            var converter = new BrushConverter();
+            if (reset)
+            {
+                listViewItem.Background = Brushes.Transparent;
+                listViewItem.Foreground = (Brush)converter.ConvertFromString("#FFDCDCDC");
+                return;
+            }
+            listViewItem.Background = (Brush)converter.ConvertFromString("#6f2be3");
+        }
+
+        /// <summary>
+        /// Set Color of listView on click. 
+        /// </summary>
+        /// <param name="listViewItem"></param>
+        /// <param name="reset"></param>
+        public static void SetListViewColorApp(ListViewItem listViewItem, bool reset)
+        {
+            if (listViewItem.IsEnabled)
+            {
+                var converter = new BrushConverter();
+                if (reset)
+                {
+                    listViewItem.Background = Brushes.Transparent;
+                    listViewItem.Foreground = (Brush)converter.ConvertFromString("#FFDCDCDC");
+                    return;
+                }
+                listViewItem.Background = (Brush)converter.ConvertFromString("#6f2be3");
+            }
+        }
+    }
+}
