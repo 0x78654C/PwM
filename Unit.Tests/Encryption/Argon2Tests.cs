06bbd3d6751ab49b313e3a7f3851fc54b46d1712
michael bourke <michael.bourke@live.co.uk>
2021-12-20 12:05:39 +0000

    Tests Made

--- Unit.Tests/Encryption/Argon2Tests.cs
+++ Unit.Tests/Encryption/Argon2Tests.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Unit.Tests.Encryption
 {
     public class Argon2Tests
     {
+        /* we are not testing logic here just results so we can refactor or swap out and ensure backward compatiablity */
+        [Theory]
+        [InlineData("gweghwre52315herhrethej", ",= ?MN????????JiR???4?????3?a?")]
+        [InlineData("herherhtrjrjj543y34y3", "??.c?????=?G???e?\\%s?nPf?:??")]
+        public void Ensure_we_can_(string inParam,string expected)
+        {
+            var bytes = PwM.Encryption.Argon2.Argon2HashPassword(inParam);
+            string asciiString = Encoding.ASCII.GetString(bytes, 0, bytes.Length);
+
+            Assert.Equal(expected, asciiString);
+        }
     }
 }
