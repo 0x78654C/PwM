9033dd0d0a17d7ad60b410ab079ca7fb2f49aa4b
0x78654C <l2xguard@live.com>
2021-12-26 00:58:48 +0200

    added import notification window for replace existing vault file

---
+++ PwM/ImportNotification.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace PwM
+{
+    /// <summary>
+    /// Interaction logic for ImportNotification.xaml
+    /// </summary>
+    public partial class ImportNotification : Window
+    {
+        public ImportNotification()
+        {
+            InitializeComponent();
+            string vault = Utils.GlobalVariables.vaultName;
+            vault = vault.Substring(0, vault.Length - 2);
+            notificationLBL.Text = $"Vault {vault} already exists. Do you want to replace it?";
+            SystemEvents.PowerModeChanged += SystemEvents_PowerModeChanged; // Exit vault on suspend.
+            SystemEvents.SessionSwitch += new SessionSwitchEventHandler(SystemEvents_SessionSwitch); // Exit vault on lock screen.
+        }
+
+        /// <summary>
+        /// Check if PC enters sleep or hibernate mode and closes window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SystemEvents_PowerModeChanged(object sender, PowerModeChangedEventArgs e)
+        {
+            switch (e.Mode)
+            {
+                case PowerModes.Suspend:
+                    Utils.GlobalVariables.importConfirmation = "";
+                    this.Close();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Check if lock screen and closes window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
+        {
+            if (e.Reason == SessionSwitchReason.SessionLock)
+            {
+                Utils.GlobalVariables.importConfirmation = "";
+                this.Close();
+            }
+        }
+
+        /// <summary>
+        /// Close button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void confirmBTN_Click(object sender, RoutedEventArgs e)
+        {
+            Utils.GlobalVariables.importConfirmation = "yes";
+            this.Close();
+        }
+
+        /// <summary>
+        /// Cancel button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CancelBTN_Click(object sender, RoutedEventArgs e)
+        {
+            Utils.GlobalVariables.importConfirmation = "";
+            this.Close();
+        }
+    }
+}
