2fca869d7be8bd60c86965a8d4e73587f401d3b4
0x78654C <l2xguard@live.com>
2021-12-28 00:43:27 +0200

    new variables added for pass secure string master password and master password check

--- PwM/Utils/GlobalVariables.cs
+++ PwM/Utils/GlobalVariables.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Security;
 
 namespace PwM.Utils
 {
     /* Global variables definitions class */
     public static class GlobalVariables
     {
+        public static SecureString masterPassword { get; set; }
         public static string applicationName { get; set; }
         public static string accountName { get; set; }
         public static string accountPassword { get; set; }
@@ -17,6 +19,7 @@ namespace PwM.Utils
         public static string gridColor { get; set; }
         public static string messageData { get; set; }
         public static bool vaultChecks = false;
+        public static bool masterPasswordCheck = true;
         private static string s_rootPath = System.IO.Path.GetPathRoot(Environment.SystemDirectory);
         private static readonly string s_accountName = Environment.UserName;
         public static readonly string passwordManagerDirectory = $"{s_rootPath}Users\\{s_accountName}\\AppData\\Local\\PwM\\";
