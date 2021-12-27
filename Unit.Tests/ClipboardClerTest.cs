78b87dbedf50a12813ab19042044d14fe03e52c2
0x78654C <l2xguard@live.com>
2021-12-28 01:02:06 +0200

    added clipboard unit test

---
+++ Unit.Tests/ClipboardClerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Xunit;
+namespace Unit.Tests
+{
+    public class ClipboardClerTest
+    {
+        [Theory]
+        [InlineData("test")]
+        public void ClearSpecificTextOnlyClipboard(string password)
+        {
+            if (Clipboard.ContainsText(TextDataFormat.Text))
+            {
+                bool checkClip = false;
+                string clipboardContent = Clipboard.GetText(TextDataFormat.Text);
+                checkClip = clipboardContent == password ? true : false;
+                Assert.True(checkClip);
+            }
+        }
+    }
+}
