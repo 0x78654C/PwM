0cd5f64e6d945612f85caa02cbb1c06f2f48b5d1
0x78654C <l2xguard@live.com>
2021-12-28 00:47:14 +0200

    added timer starter for 30 min master password window pop on add app/update account/ delete account event in applicaiton tab after login

---
+++ PwM/Utils/MasterPasswordTimerStart.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Threading;
+
+namespace PwM.Utils
+{
+    public class MasterPasswordTimerStart
+    {
+
+        /// <summary>
+        /// Master password timer check for promt every 30 minutes the master password pop window.
+        /// </summary>
+        public static void MasterPasswordCheck_Timer(DispatcherTimer masterPaswordTimer)
+        {
+            masterPaswordTimer = new DispatcherTimer();
+            masterPaswordTimer.Tick += MasterPasswordCheck_Tick;
+            masterPaswordTimer.Interval = new TimeSpan(0, 1, 0);
+            masterPaswordTimer.Start();
+        }
+
+        /// <summary>
+        /// Set flag for master password timer check.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MasterPasswordCheck_Tick(object sender, EventArgs e)
+        {
+            GlobalVariables.masterPasswordCheck = false;
+        }
+    }
+}
