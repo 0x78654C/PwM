83f9ac8ce8170b62e07014b4dd379d1cf5529e43
0x78654C <l2xguard@live.com>
2021-12-28 22:48:06 +0200

    removed new DispatcherTimer instance declaration. Is declared once in mainwindow.cs

--- PwM/Utils/MasterPasswordTimerStart.cs
+++ PwM/Utils/MasterPasswordTimerStart.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Threading;
 
 namespace PwM.Utils
@@ -13,7 +11,6 @@ namespace PwM.Utils
         /// </summary>
         public static void MasterPasswordCheck_Timer(DispatcherTimer masterPaswordTimer)
         {
-            masterPaswordTimer = new DispatcherTimer();
             masterPaswordTimer.Tick += MasterPasswordCheck_Tick;
             masterPaswordTimer.Interval = new TimeSpan(0, 30, 0);
             masterPaswordTimer.Start();
