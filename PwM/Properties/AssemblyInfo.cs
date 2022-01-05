aecc8e549069430b8c6aa3e6e958c874f804d89a
0x78654C <l2xguard@live.com>
2022-01-05 23:41:39 +0200

    changed version and year

--- PwM/Properties/AssemblyInfo.cs
+++ PwM/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PwM")]
-[assembly: AssemblyCopyright("Copyright ©  2021 0x078654c")]
+[assembly: AssemblyCopyright("Copyright ©  2022 0x078654c")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
