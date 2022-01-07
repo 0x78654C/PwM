487670e96ad1fb24cb481d96cc11e6e493009e29
0x78654C <l2xguard@live.com>
2022-01-08 01:31:30 +0200

    added global var newAccountPassword for fix issue with account password update message on blank password box and newMasterPassword var

--- PwM/Utils/GlobalVariables.cs
+++ PwM/Utils/GlobalVariables.cs
@@ -7,9 +7,11 @@ namespace PwM.Utils
     public static class GlobalVariables
     {
         public static SecureString masterPassword { get; set; }
+        public static SecureString newMasterPassword { get; set; }
         public static string applicationName { get; set; }
         public static string accountName { get; set; }
         public static string accountPassword { get; set; }
+        public static string newAccountPassword { get; set; }
         public static string deleteConfirmation { get; set; }
         public static string createConfirmation { get; set; }
         public static string importConfirmation { get; set; }
@@ -20,6 +22,7 @@ namespace PwM.Utils
         public static string gridColor { get; set; }
         public static string messageData { get; set; }
         public static bool vaultChecks = false;
+        public static bool vaultOpen = false;
         public static bool masterPasswordCheck = true;
         private static string s_rootPath = System.IO.Path.GetPathRoot(Environment.SystemDirectory);
         private static readonly string s_accountName = Environment.UserName;
