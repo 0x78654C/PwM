9425b78a65cc70e4585430024ba06e92ec7b5752
0x78654C <l2xguard@live.com>
2022-01-12 22:36:57 +0200

    added PwM command line interface

---
+++ PwM_CLI/Utils/UI.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace PwM.Utils
+{
+    public static class UI
+    {
+        /// <summary>
+        /// Write error output in color Red.
+        /// </summary>
+        /// <param name="text"></param>
+        public static void ErrorWriteLine(string text)
+        {
+            ConsoleColor currentForeground = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Error: {text}");
+            Console.ForegroundColor = currentForeground;
+
+        }
+
+        /// <summary>
+        /// Change color of a specific line in console.
+        /// </summary>
+        /// <param name="color"></param>
+        /// <param name="text"></param>
+        public static void ColorConsoleTextLine(ConsoleColor color, string text)
+        {
+            ConsoleColor currentForeground = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = currentForeground;
+        }
+
+        /// <summary>
+        /// Change color of a specific text in console.
+        /// </summary>
+        /// <param name="color"></param>
+        /// <param name="text"></param>
+        public static void ColorConsoleText(ConsoleColor color, string text)
+        {
+            ConsoleColor currentForeground = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ForegroundColor = currentForeground;
+        }
+    }
+}
