56fb03212529221e316d73a61403299710f0cddc
MkbMain <michael.bourke@live.co.uk>
2022-01-29 21:28:11 +0000

    How Random Is random

--- PwMLib/PasswordGenerator.cs
+++ PwMLib/PasswordGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Security.Cryptography;
 
 namespace PwMLib
 {
@@ -10,7 +11,6 @@ namespace PwMLib
         private const string Alphabet = "abcdefghijklmnopqrstuvwxyz";
         private const string Numbers = "0123456789";
         private const string Symbols = "`~!@#$%^&*()-_=+[]{}\\|;:'\\,<.>/?";
-        private static Random _random = new Random(Guid.NewGuid().GetHashCode());
 
         public static string GeneratePassword(int length = 16, bool useUpper = true, bool useLower = true,
             bool useSymbols = true, bool useNumbers = true)
@@ -30,19 +30,28 @@ namespace PwMLib
             collection += useUpper ? Alphabet.ToUpper() : "";
             collection += useSymbols ? Symbols : "";
 
-            return GeneratePassword(string.Join("", collection.OrderBy(r => Guid.NewGuid().GetHashCode())), 0, length, useUpper, useLower, useSymbols, useNumbers);
+            return GeneratePassword(collection.OrderBy(r => Guid.NewGuid().GetHashCode()).ToArray(), 0, length, useUpper, useLower, useSymbols, useNumbers);
         }
 
-        private static string GeneratePassword(string collection, int attempt, int length = 16, bool useUpper = true, bool useLower = true,
+        private static string GeneratePassword(char[] chars, int attempt, int length = 16, bool useUpper = true, bool useLower = true,
              bool useSymbols = true, bool useNumbers = true)
         {
-            var password = string.Join("", Enumerable.Range(0, length).Select(t => collection[_random.Next(0, collection.Length)]));
+          
+            var bytes = new byte[length * 8];
+            new RNGCryptoServiceProvider().GetBytes(bytes);
+            var result = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                ulong value = BitConverter.ToUInt64(bytes, i * 8);
+                result[i] = chars[value % (uint)chars.Length];
+            }
+            var password = string.Join("", result);
             if (length > 7 && attempt < 5 && (useLower && !password.Any(e => Alphabet.ToLower().Contains(e))) ||
                 (useSymbols && !password.Any(e => Symbols.Contains(e))) ||
                     (useNumbers && !password.Any(e => Numbers.Contains(e))) ||
                 (useUpper && !password.Any(e => Alphabet.ToUpper().Contains(e))))
             {
-                return GeneratePassword(collection, attempt + 1, length, useUpper, useUpper, useSymbols, useNumbers);
+                return GeneratePassword(chars, attempt + 1, length, useUpper, useUpper, useSymbols, useNumbers);
             }
             return password;
         }
