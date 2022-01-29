7bf17429c3af2135d1d467a2b0ea9d6d9f77ed2a
0x78654C <l2xguard@live.com>
2022-01-30 01:53:46 +0200

    added class for manage items(vault name and path) for JSON file.

---
+++ PwM/Utils/VaultDetails.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace PwM.Utils
+{
+    public class VaultDetails
+    {
+        public string VaultName { get; set; }
+        public string SharedPath { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is VaultDetails details &&
+                   VaultName == details.VaultName &&
+                   SharedPath == details.SharedPath;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1670917873;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(VaultName);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(SharedPath);
+            return hashCode;
+        }
+    }
+}
