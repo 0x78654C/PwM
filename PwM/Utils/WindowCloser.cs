375a478bf94682a55981aa8e85dbd61148463212
0x78654C <l2xguard@live.com>
2022-02-09 22:55:43 +0200

    added class for close current opened WPF window.

---
+++ PwM/Utils/WindowCloser.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace PwM.Utils
+{
+    public class WindowCloser
+    {
+        /// <summary>
+        /// Close current opened WPF window.
+        /// </summary>
+        /// <param name="windowXmlName"></param>
+        public static void CloseWindow(string windowXmlName)
+        {
+            foreach (Window w in Application.Current.Windows)
+            {
+                if (w.Name==windowXmlName)
+                {
+                    w.Close();
+                }
+            }
+        }
+    }
+}
