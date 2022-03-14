9277e94f5adcffb65a5ace3fb1dc96e3267240ba
0x78654C <l2xguard@live.com>
2022-03-14 12:51:41 +0200

    up

---
+++ PwM/Encryption/PasswordValidator.cs
@@ -0,0 +1,63 @@
+﻿using System.Security;
+using System.Text.RegularExpressions;
+
+namespace PwM.Encryption
+{
+    public static class PasswordValidator
+    {
+        /// <summary>
+        /// Password complexity check: digit, upper case and lower case.
+        /// </summary>
+        /// <param name="password">Password string.</param>
+        /// <returns>bool</returns>
+        public static bool ValidatePassword(string password)
+        {
+            const string patternPassword = @"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{12,500}$";
+            return !(string.IsNullOrEmpty(password) || CheckSpaceChar(password) || !Regex.IsMatch(password, patternPassword) || !SpecialCharCheck(password));
+        }
+
+        /// <summary>
+        /// Check string for special character.
+        /// </summary>
+        /// <param name="input">Password string.</param>
+        /// <returns></returns>
+        private static bool SpecialCharCheck(string input)
+        {
+            return input.IndexOfAny(@"\|!#$%&/()=?»«@£§€{}.-;'<>_,".ToCharArray()) > -1;
+        }
+
+        /// <summary>
+        /// Check for empty space in password.
+        /// </summary>
+        /// <param name="input">Password string.</param>
+        /// <returns></returns>
+        private static bool CheckSpaceChar(string input)
+        {
+            return input.Contains(" ");
+        }
+
+        /// <summary>
+        /// Converts the secure string to string.
+        /// </summary>
+        /// <returns>The secure string to string.</returns>
+        /// <param name="data">Data.</param>
+        public static string ConvertSecureStringToString(this SecureString data)
+        {
+            return new System.Net.NetworkCredential(string.Empty, data).Password;
+        }
+        /// <summary>
+        /// Convert string to secure string.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static SecureString StringToSecureString(string data)
+        {
+            SecureString secureString = new SecureString();
+            foreach (var c in data)
+            {
+                secureString.AppendChar(c);
+            }
+            return secureString;
+        }
+    }
+}
---
+++ PwM/Encryption/PaswordGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace PwM.Encryption
+{
+    // Cryptograhic password generator class.
+    // Credits to: mkbmain
+
+    public static class PasswordGenerator
+    {
+        private const string Alphabet = "abcdefghijklmnopqrstuvwxyz";
+        private const string Numbers = "0123456789";
+        private const string Symbols = "`~!@#$%^&*()-_=+[]{}\\|;:'\\,<.>/?";
+        private static Random _random = new Random(Guid.NewGuid().GetHashCode());
+
+        public static string GeneratePassword(int length = 16, bool useUpper = true, bool useLower = true,
+            bool useSymbols = true, bool useNumbers = true)
+        {
+            if (length < 1)
+            {
+                throw new ArgumentException($"Can not make a string of {length} length");
+            }
+
+            if (!new[] { useLower, useUpper, useSymbols, useNumbers }.Any(e => e))
+            {
+                throw new ArgumentException($"Can not make a string of {length} length while not using any chars");
+            }
+
+            var collection = useLower ? Alphabet.ToLower() : "";
+            collection += useNumbers ? Numbers : "";
+            collection += useUpper ? Alphabet.ToUpper() : "";
+            collection += useSymbols ? Symbols : "";
+
+            return GeneratePassword(string.Join("", collection.OrderBy(r => Guid.NewGuid().GetHashCode())), 0, length, useUpper, useLower, useSymbols, useNumbers);
+        }
+
+        private static string GeneratePassword(string collection, int attempt, int length = 16, bool useUpper = true, bool useLower = true,
+             bool useSymbols = true, bool useNumbers = true)
+        {
+            var password = string.Join("", Enumerable.Range(0, length).Select(t => collection[_random.Next(0, collection.Length)]));
+            if (length > 7 && attempt < 5 && (useLower && !password.Any(e => Alphabet.ToLower().Contains(e))) ||
+                (useSymbols && !password.Any(e => Symbols.Contains(e))) ||
+                    (useNumbers && !password.Any(e => Numbers.Contains(e))) ||
+                (useUpper && !password.Any(e => Alphabet.ToUpper().Contains(e))))
+            {
+                return GeneratePassword(collection, attempt + 1, length, useUpper, useUpper, useSymbols, useNumbers);
+            }
+            return password;
+        }
+    }
+}
+
