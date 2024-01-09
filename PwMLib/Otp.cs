ffa790920b4255171ea788ac6cce3c9079fa1771
xeL <xcoding.dev@gmail.com>
2024-01-09 23:12:08 +0200

    created URI link generator, console QR display, and SVG QR (work in progress)

--- PwMLib/Otp.cs
+++ PwMLib/Otp.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using OtpNet;
 using Net.Codecrete.QrCodeGenerator;
 
 namespace PwMLib
 {
-
     /// <summary>
     /// Class for generating QR codes for vaults
     /// </summary>
@@ -14,31 +11,63 @@ namespace PwMLib
     {
         private static string Name = "PwM";
 
-        //
-        public static void GenerateQR( string secret, string vaultName,string pwm)
+        /// <summary>
+        /// Generate URI code.
+        /// </summary>
+        /// <param name="secret"></param>
+        /// <param name="vaultName"></param>
+        /// <param name="pwm"></param>
+        /// <returns></returns>
+        private static string GenerateURI( string secret, string vaultName)
         {
-            var bytes =  Base32Encoding.ToBytes(secret);
+            var bytes =  Base32Encoding.ToBytes("ASDASD4454");
             var totp = new Totp(bytes);
-            var uriString = new OtpUri(OtpType.Totp, secret, vaultName, pwm).ToString();
-
+            var uriString = new OtpUri(OtpType.Totp, secret, vaultName, Name).ToString();
+            return uriString;
         }
 
-        private static void GenerateQR(string otpauth)
+
+        /// <summary>
+        ///  Generate SVG QR code
+        /// </summary>
+        /// <param name="secret"></param>
+        /// <param name="vaultName"></param>
+        /// <param name="pwm"></param>
+        private static void GenerateQRSVG(string secret, string vaultName)
         {
+            string otpauth = GenerateURI(secret, vaultName);
             var qr = QrCode.EncodeText(otpauth, QrCode.Ecc.Medium);
             string svg = qr.ToSvgString(4);
         }
 
-        static void GenerateQRDisplay(string otpauth)
+
+        /// <summary>
+        /// Generate QR code for console or editor WPF. 
+        /// </summary>
+        /// <param name="secret"></param>
+        /// <param name="vaultName"></param>
+        /// <param name="pwm"></param>
+        public static void GenerateQRDisplay(string secret, string vaultName)
         {
+            string otpauth = GenerateURI(secret, vaultName);
             var segments = QrSegment.MakeSegments(otpauth);
-            var qr = QrCode.EncodeSegments(segments, QrCode.Ecc.High, 5, 5, 2, false);
+            var qr = QrCode.EncodeSegments(segments, QrCode.Ecc.Low, 1, 40, -1, false);
+            Console.Write("\n");
+            string result = string.Empty;
             for (int y = 0; y < qr.Size; y++)
             {
                 for (int x = 0; x < qr.Size; x++)
                 {
-                    Console.Write(qr.GetModule(x, y));
+                    var c = (char)9608;
+                    if (qr.GetModule(x, y))
+                    {
+                        Console.Write(c);
+                        Console.Write(c);
+                    }
+                    else
+                        Console.Write("  ");
                 }
+                Console.Write("\n");
             }
         }
     }
