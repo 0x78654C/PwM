97e3d794a95cb5f22c20062ee770593aed1a19ae
mkbmain <michael.bourke@live.co.uk>
2024-03-24 07:27:30 +0000

    Minor Refactor Would test this but should be identical

--- PwM_CLI/Program.cs
+++ PwM_CLI/Program.cs
@@ -224,24 +224,15 @@ If you like this application and want to support the project you can always buy 
         /// </summary>
         private static void ListVaults()
         {
-            var outFiles = string.Empty;
             var getFiles = Directory.GetFiles(s_vaultsDir);
-            var filesCount = getFiles.Count();
-            foreach (var file in getFiles)
-            {
-                if (!file.EndsWith(".x")) continue;
-                var fileInfo = new FileInfo(file);
-                var vaultName = fileInfo.Name.Substring(0, fileInfo.Name.Length - 2);
-                outFiles += "----------------\n";
-                outFiles += vaultName + Environment.NewLine;
-            }
-
-            if (filesCount == 0)
+            if (!getFiles.Any())
             {
                 ColorConsoleTextLine(ConsoleColor.Yellow, "There are no vaults created!");
                 return;
             }
 
+            var names = getFiles.Select(w => new FileInfo(w)).Select(q => q.Name[..^2]);
+            var outFiles = string.Join(Environment.NewLine, names.Select(w => $"----------------\n{w}"));
             Console.WriteLine("List of current vaults:");
             Console.WriteLine(outFiles + "----------------");
         }
