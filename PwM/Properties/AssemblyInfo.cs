6824667aff2849cc049f9e5d100af857732a44cc
xeL <xcoding.dev@gmail.com>
2024-06-07 23:18:03 +0300

    Version and copyright year changed

--- PwM/Properties/AssemblyInfo.cs
+++ PwM/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PwM")]
-[assembly: AssemblyCopyright("Copyright ©  2021 - 2022 0x078654c")]
+[assembly: AssemblyCopyright("Copyright ©  2021 - 2024 0x078654c")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.4.0")]
-[assembly: AssemblyFileVersion("1.1.4.0")]
+[assembly: AssemblyVersion("1.1.5.0")]
+[assembly: AssemblyFileVersion("1.1.5.0")]
