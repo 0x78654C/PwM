c7fa18156b33459cb563c5b184cfce73d0778e9d
xeL <xcoding.dev@gmail.com>
2024-07-16 23:42:29 +0300

    created class to mage GET request from API's

---
+++ PwMLib/HttpService.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace PwMLib
+{
+    public class HttpService
+    {
+        private readonly HttpClient _client;
+
+        public HttpService()
+        {
+            HttpClientHandler handler = new HttpClientHandler
+            {
+                AutomaticDecompression = DecompressionMethods.None
+            };
+
+            _client = new HttpClient();
+        }
+
+        /// <summary>
+        /// GET request on custom API.
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <returns></returns>
+        public async Task<string> GetAsync(string uri)
+        {
+            using HttpResponseMessage response = await _client.GetAsync(uri);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
