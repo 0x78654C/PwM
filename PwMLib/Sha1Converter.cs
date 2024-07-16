b3bae2c405a8bd14cd28eec99c788410e4563562
xeL <xcoding.dev@gmail.com>
2024-07-16 23:11:04 +0300

    created class for convert string to sha1

---
+++ PwMLib/Sha1Converter.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace PwMLib
+{
+    public class Sha1Converter
+    {
+        public static string Hash(string input)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sb = new StringBuilder(hash.Length * 2);
+                foreach (byte b in hash)
+                    sb.Append(b.ToString("X2"));
+                return sb.ToString();
+            }
+        }
+    }
+}
