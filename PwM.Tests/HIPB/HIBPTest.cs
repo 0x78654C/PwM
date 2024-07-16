9f098ad8876e78c1fa7c177b99f9760dfaaa4cc6
xeL <xcoding.dev@gmail.com>
2024-07-16 23:45:27 +0300

    created test cases for HIBP class

---
+++ PwM.Tests/HIPB/HIBPTest.cs
@@ -0,0 +1,16 @@
+﻿using PwMLib;
+
+namespace PwM.Tests.HIPB
+{
+    public class HIBPTest
+    {
+        [Theory]
+        [InlineData("pawn","336")]
+        public void Check_API_HIBP(string password, string tBreaches)
+        {
+            var hibp = new HIBP();
+            var totalBreaches = hibp.CheckIfPwnd(password);
+            Assert.Equal(totalBreaches,tBreaches);
+        }
+    }
+}
