347871a8d071cc7103a3a4003f92c61d58c787b1
xeL <xcoding.dev@gmail.com>
2024-07-20 23:07:22 +0300

    removed unused parameter for the test

--- PwM.Tests/HIPB/HIBPTest.cs
+++ PwM.Tests/HIPB/HIBPTest.cs
@@ -5,8 +5,8 @@ namespace PwM.Tests.HIPB
     public class HIBPTest
     {
         [Theory]
-        [InlineData("pawn","336")]
-        public void Check_API_HIBP(string password, string tBreaches)
+        [InlineData("pawn")]
+        public void Check_API_HIBP(string password)
         {
             var hibp = new HIBP(GlobalVariables.apiHIBP);
             var totalBreaches = hibp.CheckIfPwnd(password).Result;
